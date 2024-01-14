0c7da50a8af33ae0f3ab19108bc24095f724b0c7
3Alan <1540703192@qq.com>
2024-01-14 21:04:59 +0800

    feat: update prompt

--- utils/summarizer.js
+++ utils/summarizer.js
@@ -12,7 +12,7 @@ async function summarizer(article) {
     }
   });
 
-  const prompt = `Summarize the following markdown in Chinese. Try to use your own words when possible. Keep your answer under 5 sentences, remove any unnecessary information, whitespace.
+  const prompt = `Summarize the following markdown in Chinese. Try to use your own words when possible. Keep your answer under 5 sentences, don't use markdown syntax.
   
   ${article}
   `;
